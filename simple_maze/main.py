bdb0e2900c2e12e5c620818de4985d62049f0e44
Tsukasa Ebuchi <tsukasa_ebuchi@demia.co.jp>
2024-09-23 19:03:44 +0900

    インポート用初期コード

---
+++ simple_maze/main.py
@@ -0,0 +1,17 @@
+def main():
+    
+    # 何回ゴールするか
+    EPISODE_MAX = 50
+    # ゴールまでの打ち切りステップ数
+    STEP_MAX = 3000
+    # 学習率
+    LEARNING_RATE = 0.2
+    # 割引率
+    DISCOUNT_RATE = 0.95
+    # 描画スピード
+    SLEEP_TIME = 0.1
+    
+    #MazeとQLearningを用いた処理を記述
+    
+if __name__ == "__main__":
+    main()
---
+++ simple_maze/maze.py
@@ -0,0 +1,32 @@
+BOARD = [
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+]
+
+class Maze:
+    def __init__(self):
+        pass
+    
+    def is_goal(self):
+        pass
+    
+    def move(self):
+        pass
+    
+    def draw(self):
+        pass
+
+    def reset(self):
+        pass
+    
+    def get_position(self):
+        pass
+    
+    def board_size(self):
+        pass
+    
+    

32404cf86fd9f3167553119d9054f43a8753004b
EbuchiTsukasa <tsukasa_ebuchi@demia.co.jp>
2024-10-17 15:31:49 +0900

    Q学習に必要な条件を削除

--- simple_maze/main.py
+++ simple_maze/main.py
@@ -5,21 +5,6 @@ from q_learning import QLearning
 
 def main():
     
-    ## Q学習で必要な条件
-    
-    # 何回ゴールするか
-    EPISODE_MAX = 50
-    # ゴールまでの打ち切りステップ数
-    STEP_MAX = 3000
-    # 学習率
-    LEARNING_RATE = 0.2
-    # 割引率
-    DISCOUNT_RATE = 0.95
-    # 描画スピード
-    SLEEP_TIME = 0.015
-    
-    # MazeとQLearningを用いた処理を記述
-    
     maze = Maze()
     
     maze.draw()
